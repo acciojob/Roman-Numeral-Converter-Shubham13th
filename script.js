a81849bb5c8eaf866ce31f280758edab1d0d5cbb
Shubham Sharma <112186685+Shubham13th@users.noreply.github.com>
2025-08-22 12:45:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,45 @@ function convertToRoman(num) {
     };
 
   //your code here
+	let result = "";
+
+  for (let key in obj) {
+    let [symbol, value] = obj[key];
+
+    // handle subtractive cases first
+    if (symbol === "C" && num >= 900) {  // CM
+      result += "CM";
+      num -= 900;
+    }
+    if (symbol === "C" && num >= 400) {  // CD
+      result += "CD";
+      num -= 400;
+    }
+    if (symbol === "X" && num >= 90) {   // XC
+      result += "XC";
+      num -= 90;
+    }
+    if (symbol === "X" && num >= 40) {   // XL
+      result += "XL";
+      num -= 40;
+    }
+    if (symbol === "I" && num >= 9) {    // IX
+      result += "IX";
+      num -= 9;
+    }
+    if (symbol === "I" && num >= 4) {    // IV
+      result += "IV";
+      num -= 4;
+    }
+
+    // normal case
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
+  }
+
+  return result;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
